1613a98ee3679f0c09e2542d94197b30738f69b0
FocusedFaust <quieto297@gmail.com>
2025-09-17 10:47:05 +0200

    [FIX] Storage works and checked boxes are restored upon reload

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
 console.log("The extension is up and running");
 
 var headings = document.querySelectorAll("h3")
-const localData = {}
 
 for (head of headings){
     //console.log(head.lastChild.textContent)
     var checkbox = document.createElement('input')
     checkbox.type = "checkbox"
-    checkbox.name = "work_tracker"
+    checkbox.id = "work_tracker"
     checkbox.style.display = 'block'
-    checkbox.checked = localData[head.lastChild.textContent]
+    //if (localStorage[head.lastChild.textContent] == true) {console.log("true")}
+    checkbox.checked = JSON.parse(localStorage.getItem('data'))[head.lastChild.textContent]
     //checkbox.onclick = checkboxClicked(checkbox, head)
-    checkbox.addEventListener("click", () => checkboxClicked(checkbox, head))
+    checkbox.onclick = allCheckboxes
+    //checkbox.addEventListener("click", () => checkboxClicked(checkbox, head))
 
     head.insertAdjacentElement('afterbegin', checkbox)  // On top of
     //head.appendChild(checkbox)                        // Below
 }
 
-function checkboxClicked(box, parent) {
-    console.log(box.checked)
+function allCheckboxes() {
+    var boxes = document.querySelectorAll("#work_tracker")
+    console.log("allCheckboxes")
     // Fetch data from the storage or create it if nonexistant
     var fetched = JSON.parse(localStorage.getItem("data"))
     if (fetched == null) {
-        localStorage.setItem("data", JSON.stringify(localData))
+        localStorage.setItem("data", JSON.stringify({}))
         fetched = JSON.parse(localStorage.getItem("data"))
     }
 
     // Set data
-    fetched[parent.lastChild.textContent] = box.checked
-    localStorage.setItem("data", JSON.stringify(fetched))
+    for (box of boxes) {
+        fetched[box.parentElement.lastChild.textContent] = box.checked
+        localStorage.setItem("data", JSON.stringify(fetched))
+    }
 }
